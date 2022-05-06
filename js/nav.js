e4b83f9102881baa6822cf8ce0a43e8cad221d0e
Terje Rønning <t.ronning@gmail.com>
2022-05-06 11:19:33 +0200

    endret layout
    endret alt...

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,13 @@
+/* 
 document.getElementById("myHead").innerHTML =
     "<span id='headerText'>Mine prosjekter</span>"
 	+ "<span id='headerSubtext'>Subtitle</span>";
-    
+*/    
 document.getElementById("myNav").innerHTML =
 	"<ul id='navLinks'>"
-	+ "<li><a class='active' ref='index.html'>Startsiden</a></li>"
+	+ "<li><a href='index.html'>Startsiden</a></li>"
 	+ "<li><a href='samleside.html'>Samleside læring</a>"
-	+ "<li><a href='hjemmeside.html'>Moodboard</a>"
+	+ "<li><a href='moodboard.html'>Moodboard</a>"
 	+ "<li><a href='hjemmeside.html'>Aktiv Læring hjemmeside</a>"
 	+ "<li><a href='matbit.html'>MAT B-I-T prosjekt</a>"
 	+ "<li><a href='api.html'>Javascript API</a></li>"
@@ -16,11 +17,11 @@ document.getElementById("myNav").innerHTML =
 document.getElementById("myFooter").innerHTML =
 	"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + " All"
 	+ " rights reserved.</p>"
-	+ "<p id='credits'>Layout by You</p>"
 	+ "<p id='contact'><a href='mailto:t.ronning@gmail.comm'>Kontakt meg</a></p>"
 
 
 // identify active 
+// obs! må beholde menyen, ikke laste full sside
     var a = document.querySelectorAll(".navLinks a");
     for (var i = 0, length = a.length; i < length; i++) {
       a[i].onclick = function() {
@@ -30,3 +31,12 @@ document.getElementById("myFooter").innerHTML =
       };
     }
      
+	/* ny metode
+	document.location.pathname
+Se hvilken siden du er på
+	hvis filen har denne endelsen, legg til klasse acitive
+
+	alt:
+	kun 1 index.html
+	laste kun innhold
+	*/
